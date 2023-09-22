47dd9ecb84b726ae9a998acff990cb77c0765c25
granja-c <112727067+granja-c@users.noreply.github.com>
2023-09-22 14:17:40 +0000

    stuff

---
+++ msoe141.py
@@ -0,0 +1,17 @@
+def main():
+  wrd = input("Enter a string: ")
+  wrd = wrd.lower()
+  cnt = 0
+  if len(wrd) == 0 or len(wrd) == 1:
+    print("No double letters")
+
+  else:
+    for i in range(0, len(wrd) - 1):
+      if wrd[i] == wrd[i+1]:
+        cnt += 1
+
+  print(f"The word '{wrd}' has {cnt} double letters")
+  pass
+
+if __name__ == "__main__":
+  main()
